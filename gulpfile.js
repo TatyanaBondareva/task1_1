d2ff5a7cdbd676ac80266516df9cf89e284ddef0
TatyanaBondareva <tbonda1995@gmail.com>
2017-06-01 14:49:07 +0300

    commit_1_06_17
    *

--- gulpfile.js
+++ gulpfile.js
@@ -4,59 +4,80 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var watch =require('gulp-watch');
-var nunjucks = require('gulp-nunjucks');
+var njkRender = require('gulp-nunjucks-render');
+var prettify = require('gulp-html-prettify');
 var browserSync = require('browser-sync');
 var notify = require('gulp-notify');
 var scsslint = require('gulp-scss-lint');
 var reload = browserSync.reload;
 var paths = {
   html:['index.html'],
-  css:['main.scss']
-}
+  scss:['main.scss']
+};
+
+
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  gulp.src('src/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('dist/'));
 });
-gulp.task('stream', function() {
-    return watch('css/**/*.css', {ignoreInitial: false})
+
+gulp.task('stream', ['sass'], function() {
+    return watch('src/styles/*.scss', {ignoreInitial: false})
     .pipe(gulp.dest('build'));
 });
+
 gulp.task('callback', function() {
-  return watch('css/**/*.css', function() {
-    gulp.src('css/**/*.css')
+  return watch('src/styles/*.scss', function() {
+    gulp.src('src/styles/*.scss')
     .pipe(gulp.dest('build'));
   });
 });
+
 gulp.task('scss-lint', function() {
-  return gulp.src('/scss/*.scss')
+  return gulp.src('src/styles/*.scss')
   .pipe(scsslint());
 });
+gulp.task('nunjucks', function() {
+	return gulp.src('src/index.njk')
+		.pipe(njkRender())
+		.pipe(prettify({indent_char: ' ', indent_size: 2}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/**/*.njk', ['nunjucks']);
+});
+
+gulp.task('default', ['nunjucks', 'watch']);
 
 gulp.task('html', function() {
-  gulp.src(path.html)
+  gulp.src(paths.html)
   .pipe(reload({stream: true}));
 });
-gulp.task('mincss', function() {
-  return gulp.src(path.css)
+
+gulp.task('minscss', function() {
+  return gulp.src(paths.scss)
   .pipe(sass().on('error', sass.logError))
   .pipe(minifyCss())
   .pipe(gulp.dest('main'))
   .pipe.reload({stream: true});
 });
+
 gulp.task('browserSync', function() {
   browserSync ({
     server: {
-      basedir: './'
+      baseDir: 'dist/'
     },
     port: 8080,
     open: true,
     notify: false
   });
 });
+
 gulp.task('watcher', function() {
-  gulp.watch(paths.css, ['mincss']);
+  gulp.watch(paths.scss, ['minscss']);
   gulp.watch(paths.html, ['html']);
 });
 
-gulp.task('default', ['watcher', 'browserSync']);
+gulp.task('default', ['nunjucks', 'watcher', 'browserSync']);
